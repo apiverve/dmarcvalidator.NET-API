f3eb9c037bcdcc90c651cf68616950331acaccd6
APIVerve Team <hello@apiverve.com>
2025-04-23 11:23:54 -0500

    Bug Fixes

---
+++ DMARCValidatorQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class DMARCValidatorQueryOptions {
+public string domain { get; set; }
+}
+
+}
